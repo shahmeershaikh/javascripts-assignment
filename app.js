a7fa044df85b6ffb8a26461a84b6a6ee5d34aa44
Sarfaraz Shaikh <154805542+shahmeershaikh@users.noreply.github.com>
2024-02-24 12:44:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let num1 = prompt("Enter first no.");
+
+let num2 = prompt("Enter second no2.");
+
+//allowing string to be shified into
+num1 = Number(num1);
+num2 = Number(num2);
+
+//adition
+
+let sum = num1 + num2;
+alert("Addition: " + sum);
+console.log("Addition: " + sum);
+
+//substraction
+
+let minus = num1 - num2;
+alert("substraction: " + minus);
+console.log("substraction: " + minus);
+
+//multipilication
+
+let multiply = num1 * num2;
+alert("multipilication: " + multiply);
+console.log("multipilication: " + multiply);
+
+//Division
+
+let divide = num1 / num2;
+alert("Division: " + divide);
+console.log("Division: " + divide);
+
